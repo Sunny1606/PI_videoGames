a80ffb80fc348f05e1230db8c568eb837a6e5927
Sunny1606 <solescobares@gmail.com>
2023-10-25 07:34:05 -0300

    done

--- App/src/handlers/postVideogames.js
+++ App/src/handlers/postVideogames.js
@@ -1,2 +1,21 @@
+const router = require("../routes");
+const { Videogame, Genres } = require("../models");
+const { Router } = require("express");
+//crear un juego nuevo en la base de datos , recibe por body
 
-//crear un juego nuevo , recibe por body 
+router.post("/videogames", async (req, res) => {
+  try {
+    const { name, genres } = req.body;
+
+    const newVideogame = await Videogame.Create({ name });
+    if (genres && genres.length > 0) {
+
+      const genresExist = await Genres.findAll({ where: { name: genres }});
+      await newVideogame.setGenres(genresExist);
+
+    }
+    res.json(newVideogame);
+  } catch (error) {
+    res.status(500).json({ error: 'Hubo un error al crear el videojuego.' });
+  }
+});
